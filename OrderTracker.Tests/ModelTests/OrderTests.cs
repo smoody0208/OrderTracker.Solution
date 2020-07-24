f231afc43df7de6eb06dcebcd51cf0ec6c3418e1
Spencer Moody <spencer.moody@outlook.com>
2020-07-24 14:33:30 -0700

    Fail testing for GetType

--- OrderTracker.Tests/ModelTests/OrderTests.cs
+++ OrderTracker.Tests/ModelTests/OrderTests.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OrderTracker.Models;
 
-namespace VendorsPage.Tests
+namespace OrderTracker.Tests
 {
   [TestClass]
   public class OrderTests : IDisposable
@@ -18,6 +18,19 @@ namespace VendorsPage.Tests
     {
       Order newOrder = new Order("Banana Bread", 10, 10/10/10, 1000);
       Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+
+    [TestMethod]
+    public void GetType_ReturnsType_String()
+    {
+      string typeOfBread = "Banana Bread";
+      string typeOfBread1 = "Not a banana bread";
+      int quantity = 10;
+      int date = 10/10/10;
+      int price = 1000;
+      Order newOrder = new Order(typeOfBread, quantity, date, price);
+      string result = newOrder.TypeOfBread;
+      Assert.AreEqual(typeOfBread1, result);
     }
     
   }
