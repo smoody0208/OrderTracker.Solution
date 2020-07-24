6a08db9ebbe3f0beb6f399ac65b9deffe70d965e
Spencer Moody <spencer.moody@outlook.com>
2020-07-24 14:05:42 -0700

    Add order detail view and controller

--- OrderTracker/Controllers/VendorsController.cs
+++ OrderTracker/Controllers/VendorsController.cs
@@ -20,6 +20,12 @@ namespace OrderTracker.Controllers
     {
       return View();
     }
-
+    
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName, string vendorAddress)
+    {
+      Vendor newVendor = new Vendor(vendorName, vendorAddress);
+      return RedirectToAction("Index");
+    }
   }
 }    
