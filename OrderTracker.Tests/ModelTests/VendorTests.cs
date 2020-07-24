35d9401c08ae7d33fc9dd40e127b35721999cd6d
Spencer Moody <spencer.moody@outlook.com>
2020-07-24 10:27:45 -0700

    Failed testing for returning name and address

--- OrderTracker.Tests/ModelTests/VendorTests.cs
+++ OrderTracker.Tests/ModelTests/VendorTests.cs
@@ -14,5 +14,21 @@ namespace OrderTracker.Tests
       Vendor newVendor = new Vendor("Some Vendor Test", "Some Address Test");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
+
+    [TestMethod]
+    public void GetNameAndAddress_ReturnsNameAndAddress_String()
+    {
+      string vendorAddress = "P. Sherman, 42 Wallaby Way, Sydney";
+      string vendorName = "Finding Nemo";
+      string vendorAddress2 = "123 Why Worry Lane";
+      string vendorName2 = "It's All Good Bakery";
+      Vendor newVendor2 = new Vendor (vendorName, vendorAddress);
+      Vendor newVendor = new Vendor (vendorName2, vendorAddress2);
+
+      string result = newVendor.VendorName;
+
+      Assert.AreEqual(vendorName, result);
+
+    }
   }
 }
