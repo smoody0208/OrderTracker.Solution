8b610a10da5841ec8ddee0eebbdf5957e8d3241c
Spencer Moody <spencer.moody@outlook.com>
2020-07-24 08:46:55 -0700

    Update file tree

--- OrderTracker/Startup.cs
+++ OrderTracker/Startup.cs
@@ -35,7 +35,7 @@ namespace FriendLetter
       
       app.Run(async (context) =>
       {
-        await context.Response.WriteAsync("Hello World!");
+        await context.Response.WriteAsync("Something went wrong!");
       });
     }
   }  
