44d7a7661f0fd511baf1c4950fa759c9028527da
Spencer Moody <spencer.moody@outlook.com>
2020-07-24 16:36:38 -0700

    Add styling using _layout

--- OrderTracker/Startup.cs
+++ OrderTracker/Startup.cs
@@ -25,6 +25,7 @@ namespace FriendLetter
 
     public void Configure(IApplicationBuilder app)
     {
+      app.UseStaticFiles(); 
       app.UseDeveloperExceptionPage();
       app.UseMvc(routes =>
       {
